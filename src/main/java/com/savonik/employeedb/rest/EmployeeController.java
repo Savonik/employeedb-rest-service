5f8d479d815b6ec9676f4c596b6112914a8dfad3
Savonik <savonik3636@gmail.com>
2020-10-02 01:00:04 +0300

    refactoring

--- src/main/java/com/savonik/employeedb/rest/EmployeeController.java
+++ src/main/java/com/savonik/employeedb/rest/EmployeeController.java
@@ -3,11 +3,12 @@ package com.savonik.employeedb.rest;
 import com.savonik.employeedb.dto.Employee;
 import com.savonik.employeedb.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.http.ResponseEntity.*;
 
 @RequestMapping("/employees")
 @RestController
@@ -23,14 +24,14 @@ public class EmployeeController {
     @GetMapping
     public ResponseEntity<List<Employee>> getAll() {
         List<Employee> employees = employeeService.findAll();
-        return ResponseEntity.ok(employees);
+        return ok(employees);
     }
 
     @PostMapping
     public ResponseEntity<Object> addNewEmployee(@RequestBody Employee newEmployee) {
         int addResult = employeeService.addEmployee(newEmployee);
         return addResult == 0 ?
-                ResponseEntity.notFound().build() : ResponseEntity.ok().build();
+                notFound().build() : ok().build();
     }
 
     @GetMapping("/{id}")
@@ -39,22 +40,22 @@ public class EmployeeController {
         try {
             employee = employeeService.findById(id);
         } catch (NullPointerException e) {
-            return ResponseEntity.notFound().build();
+            return notFound().build();
         }
-        return ResponseEntity.ok(employee);
+        return ok(employee);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteEmployee(@PathVariable(value = "id") Long id) {
         int deleteResult = employeeService.deleteEmployee(id);
         return deleteResult == 0 ?
-                ResponseEntity.notFound().build() : ResponseEntity.ok().build();
+                notFound().build() : ok().build();
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Object> updateEmployee(@RequestBody Employee employeeDetails, @PathVariable Long id) {
         int updateResult = employeeService.updateEmployee(employeeDetails, id);
         return updateResult == 0 ?
-                ResponseEntity.notFound().build() : ResponseEntity.ok().build();
+                notFound().build() : ok().build();
     }
 }
