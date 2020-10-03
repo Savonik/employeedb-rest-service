97346f597ed88ab5241b6c5da34495789d4b542e
Savonik <savonik3636@gmail.com>
2020-10-03 03:30:45 +0300

    refactoring

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
 create table employee
 (
     employee_id   serial primary key,
-    first_name    character(50),
-    last_name     character(50),
-    department_id integer,
-    job_title     character(50),
-    gender        character(50) CHECK(gender='MALE' OR gender='FEMALE'),
-    date_of_birth date
+    first_name    character(50) NOT NULL,
+    last_name     character(50) NOT NULL,
+    department_id integer NOT NULL,
+    job_title     character(50) NOT NULL,
+    gender        character(50) CHECK (gender = 'MALE' OR gender = 'FEMALE'),
+    date_of_birth date NOT NULL
 );
 
--- src/test/java/com/savonik/employeedb/service/EmployeeServiceTest.java
+++ src/test/java/com/savonik/employeedb/service/EmployeeServiceTest.java
@@ -3,6 +3,7 @@ package com.savonik.employeedb.service;
 import com.savonik.employeedb.dao.EmployeeDao;
 import com.savonik.employeedb.dto.Employee;
 import com.savonik.employeedb.dto.Gender;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -16,23 +17,85 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EmployeeServiceTest {
+
     @Mock
     private EmployeeDao employeeDao;
 
     @InjectMocks
     private EmployeeService employeeService;
 
+    private List<Employee> testEmployees;
+    private Employee testEmployee;
+
+    @Before
+    public void beforeTest() {
+        testEmployees = new ArrayList<>();
+        testEmployees.add(new Employee(1L, "Ivan", "Ivanov",
+                1L, "programmer", Gender.MALE, new Date()));
+        testEmployees.add(new Employee(2L, "Stepan", "Stepanov",
+                2L, "data engineer", Gender.MALE, new Date()));
+
+        testEmployee = new Employee(5L, "Arsen", "Taliev",
+                3L, "data engineer", Gender.MALE, new Date());
+    }
+
     @Test
     public void findAllTest() {
-        List<Employee> returnedEmployees = new LinkedList<>();
-        returnedEmployees.add(new Employee(1L, "Ivan", "Ivanov",
-                1L, "programmer", Gender.FEMALE, new Date()));
+        Mockito.when(employeeDao.findAll()).thenReturn(testEmployees);
 
-        Mockito.when(employeeDao.findAll()).thenReturn(returnedEmployees);
-
-        List<Employee> employees = employeeService.findAll();
-        assertThat(employees).isEqualTo(returnedEmployees);
+        List<Employee> returnedEmployees = employeeService.findAll();
+        assertThat(returnedEmployees).isEqualTo(testEmployees);
 
         Mockito.verify(employeeDao).findAll();
+    }
+
+    @Test
+    public void findByIdTest() {
+        Mockito.when(employeeDao.findById(1L)).thenReturn(testEmployees);
+
+        List<Employee> returnedEmployee = employeeService.findById(1L);
+        assertThat(returnedEmployee).isEqualTo(testEmployees);
+
+        Mockito.verify(employeeDao).findById(1L);
+    }
+
+    @Test
+    public void NotFindByIdTest() {
+        Mockito.when(employeeDao.findById(1L)).thenReturn(Collections.emptyList());
+
+        List<Employee> returnedEmployee = employeeService.findById(1L);
+        assertThat(returnedEmployee).isEqualTo(Collections.emptyList());
+
+        Mockito.verify(employeeDao).findById(1L);
+    }
+
+    @Test
+    public void deleteEmployeeTest() {
+        Mockito.when(employeeDao.deleteEmployee(1L)).thenReturn(1);
+
+        int deleteStatus = employeeService.deleteEmployee(1L);
+        assertThat(deleteStatus).isEqualTo(1);
+
+        Mockito.verify(employeeDao).deleteEmployee(1L);
+    }
+
+    @Test
+    public void updateEmployeeTest() {
+        Mockito.when(employeeDao.updateEmployee(testEmployee, 1L)).thenReturn(1);
+
+        int updateStatus = employeeService.updateEmployee(testEmployee, 1L);
+        assertThat(updateStatus).isEqualTo(1);
+
+        Mockito.verify(employeeDao).updateEmployee(testEmployee, 1L);
+    }
+
+    @Test
+    public void NotUpdateEmployeeTest() {
+        Mockito.when(employeeDao.updateEmployee(testEmployee, 1L)).thenReturn(0);
+
+        int updateStatus = employeeService.updateEmployee(testEmployee, 1L);
+        assertThat(updateStatus).isEqualTo(0);
+
+        Mockito.verify(employeeDao).updateEmployee(testEmployee, 1L);
     }
 }
